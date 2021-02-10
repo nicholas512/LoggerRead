823acf2ef1ecabfa277f21d17917db31caf8014b
nicholas512 <15223327+nicholas512@users.noreply.github.com>
2021-02-10 15:52:01 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
 from setuptools import setup
 
-__version__ = "0.0.2"
+__version__ = "0.1.0"
 
 setup(
     name='LoggerReader',
     version=__version__,
     description='A simple module for reading various datalogger output files',
     long_description=open('README.md').read(),
-    # url='https://github.com/nicholas512/Pyrmafrost',
+    long_description_content_type='text/markdown',
+    url='https://github.com/nicholas512/LoggerRead',
     author='Nick Brown',
-    author_email='',
-    python_requires='>3.5.2',
+    python_requires='>3.6',
     py_modules=['LoggerReader'],
     install_requires=['pandas'],
     # test_suite='test_readers',
     classifiers=[
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Development Status :: 4 - Beta'          
     ],
     include_package_data=True,
     package_data={'LoggerReader': ['sample_files/*']}
